9c2c848842eff3af9dd7beac9d23e4ca8a8da9cf
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-11-07 15:13:38 +0100

    nolibc: add support for fchdir()
    Add support for the file descriptor based variant of chdir().
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Acked-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- sys.h
+++ sys.h
@@ -137,6 +137,7 @@ void *sbrk(intptr_t inc)
 
 /*
  * int chdir(const char *path);
+ * int fchdir(int fildes);
  */
 
 static __attribute__((unused))
@@ -149,6 +150,18 @@ static __attribute__((unused))
 int chdir(const char *path)
 {
 	return __sysret(sys_chdir(path));
+}
+
+static __attribute__((unused))
+int sys_fchdir(int fildes)
+{
+	return my_syscall1(__NR_fchdir, fildes);
+}
+
+static __attribute__((unused))
+int fchdir(int fildes)
+{
+	return __sysret(sys_fchdir(fildes));
 }
 
 
